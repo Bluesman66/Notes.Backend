9939e2ab240787e42aeb04d479cf197b1acbd8e1
Alex Paklin <alexander.pak@prpl.global>
2021-12-05 19:36:07 +0500

    add database context to project

---
+++ Notes.Backend/Notes.Persistence/INotesDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+
+namespace Notes.Persistence
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+    }
+}
